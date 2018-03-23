f3a163467c1b81167233cfa028fd8f308ad3af71
Emily97 <N00150623@student.iadt.ie>
2018-03-23 19:25:11 +0000

    changing code

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 var map;
 var myLatLng;
 var infoWindow;
+var searchBox;
 
 $(document).ready(function(){
 
@@ -66,13 +67,58 @@ $(document).ready(function(){
         google.maps.event.addListener(map, 'click', function() {
 				infoWindow.close();
 		});
+        /*
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          marker.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            marker.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+        */
     }
 
     //nearby search
     function nearbySearch(myLatLng) {
         var request = {
             location: myLatLng,
-            radius: '50000',
+            radius: "50000",
             name: 'public bathrooms'
         };
 
@@ -114,7 +160,7 @@ $(document).ready(function(){
 
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
+    searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
